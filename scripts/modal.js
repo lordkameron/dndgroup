3bb29dfdbf21b1fd887ed7eb2477442c8bd8a921
lordkameron <kameron.watson@outlook.com>
2021-03-23 17:51:11 -0700

    Uploaded new character cards. Added new character slides. Updated the Episode list

--- scripts/modal.js
+++ scripts/modal.js
@@ -45,7 +45,7 @@ var episodes = [
   {
 	name: '6',
 	episodeNumber: 'ep6',
-	subtext: 'Let\'s Make A Deal Part 1',
+	subtext: 'Let\'s Make A Deal - Part 1',
     description: 'After sacrificing himself to save Vincent, Ciaran\'s death hangs heavily on the group. Unsatisfied, they search fruitlessly for a way to revive their fallen companion, until they come across a "friendly" forest deity known as "Fir".',
     airDate: '2021-01-02',
     urlLink: ''  
@@ -53,7 +53,7 @@ var episodes = [
   {
 	name: '7',
 	episodeNumber: 'ep7',
-	subtext: 'Let\'s Make A Deal Part 2',
+	subtext: 'Let\'s Make A Deal - Part 2',
     description: 'The gang has been transported to a bizarre place! The sky is red, the moon is half gone, and they have 24 hours to find a "High Daemon Heart" before Ciaran dies.',
     airDate: '2021-01-11',
     urlLink: ''  
@@ -69,7 +69,7 @@ var episodes = [
   {
 	name: '9',
 	episodeNumber: 'ep9',
-	subtext: 'The Disk Most Black Part 1',
+	subtext: 'The Disk Most Black - Part 1',
     description: 'The Babylon crew has some time to cool down for this adventure. Phey, with the help of her aunts, takes a mental "spirit walk" into the memories of Ciaran. Malkita and Alexander take a trip down to the library of Hell. And Vincent... fails a math test.',
     airDate: '2021-01-29',
     urlLink: ''  
@@ -77,7 +77,7 @@ var episodes = [
   {
 	name: '10',
 	episodeNumber: 'ep10',
-	subtext: 'The Disk Most Black Part 2',
+	subtext: 'The Disk Most Black - Part 2',
     description: 'A strange black disc has been making the rounds in St. Augustine. Its believed by many teens and college students to hold some of the strangest, newest music ever recorded. But unknown to them, it is also host to an evil spirit dead set on taking over the world!',
     airDate: '2021-02-05',
     urlLink: ''  
@@ -85,7 +85,7 @@ var episodes = [
   {
 	name: '11',
 	episodeNumber: 'ep11',
-	subtext: 'The Disk Most Black Part 3',
+	subtext: 'The Disk Most Black - Part 3',
     description: 'The Disk has been found, cornered, and captured. The next step, banishing it back to Hell!',
     airDate: '2021-02-12',
     urlLink: ''  
@@ -93,7 +93,7 @@ var episodes = [
   {
 	name: '12',
 	episodeNumber: 'ep12',
-	subtext: 'The King of the Beach Part 1',
+	subtext: 'The King of the Beach - Part 1',
     description: 'The crew decides to take a day off from Monster Hunting and takes a trip to Vilano Beach. They chose a fortuitous time too, as its time for the "King of the Beach" competition. A fun-summer-sun competition where teams gather together to prove they are the "Kings" of the Beach. Woohoo! Beach Episode!!!!',
     airDate: '2021-02-19',
     urlLink: ''  
@@ -101,25 +101,33 @@ var episodes = [
   {
 	name: '13',
 	episodeNumber: 'ep13',
-	subtext: 'The King of the Beach Part 2',
-    description: 'The ',
+	subtext: 'The King of the Beach - Part 2',
+    description: 'Trouble reared its ugly head after the crew won the King of the Beach competition. Ciaran tried to kill JB and made a run for it, Malkita are being accosted by an Angel sent from heaven, and Vincent just can\'t seem to get a chance alone with Alice! At least they have the rest of their vacation on Colodaville to look forward too right?',
     airDate: '2021-02-26',
     urlLink: ''  
   },
   {
 	name: '14',
 	episodeNumber: 'ep14',
-	subtext: 'The King of the Beach Part 3',
-    description: 'TO BE FILLED',
+	subtext: 'The King of the Beach - Part 3',
+    description: 'CHAOS IS PARADISE! The mysterious JB has been a thrall of Fir the whole time and his using his tropical themepark as an all-you-can-eat human buffet! Will "Werewolf" be added to the menu?',
     airDate: '2021-03-05',
     urlLink: ''  
   },
   {
 	name: '15',
 	episodeNumber: 'ep14',
-	subtext: 'Babylon Boogie Revolution Max',
-    description: 'TO BE FILLED',
-    airDate: '2021-03-20',
+	subtext: 'Babylon Boogie Revolution Max - Part 1',
+    description: 'The new hotness of video games has made its way to St. Augustine! But the game is proving to be a little too-addicting, leaving some in the hospital from exhaustion and malnourishment. I hope the Babylon crew can figure this out before they Dance to their Death!',
+    airDate: '2021-03-19',
+    urlLink: ''  
+  },
+  {
+	name: '16',
+	episodeNumber: 'ep14',
+	subtext: 'Babylon Boogie Revolution Max - Part 2',
+    description: 'Alack! It has been revealed that a trickster god of the Fae is behind the magical game. After being displaced from his forest hunting grounds, the Fae decides to reap his revenge on the populace of St. Augustine. Can the Babylon crew stop him before he has the whole world Dancing Mad?',
+    airDate: '2021-03-26',
     urlLink: ''  
   }
 ]
